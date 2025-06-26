901d360a12a03a5a3cdc35ddd30ea20267a1e986
vivekbargude <vivekbargude1206@gmail.com>
2025-06-26 22:44:46 +0530

    bit manipulation

---
+++ DS/BitManipulation/bitmanipulation.java
@@ -0,0 +1,22 @@
+package DS.BitManipulation;
+
+//Functions:
+//1. No of 1 bits(set bits)
+//2. Check whether the number is power of two or not
+
+
+ class bitmanipulation {
+        public int hammingWeight(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) == 1) {
+                res += 1;
+            }
+        }
+        return res;        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
