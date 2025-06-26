ad4dffc7b16a7691cfdcce42afde8cfa45d1c677
vivekbargude <vivekbargude1206@gmail.com>
2025-06-26 22:45:03 +0530

    bit manipulation

--- DS/BitManipulation/bitmanipulation.java
+++ DS/BitManipulation/bitmanipulation.java
@@ -15,6 +15,12 @@ package DS.BitManipulation;
         }
         return res;        
     }
+
+    public boolean isPowerOfTwo(int n) {
+        if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1;
+    }
     public static void main(String[] args) {
         
     }
