76b87117915fcf473c80ca7b96db5b5931204262
vivekbargude <vivekbargude1206@gmail.com>
2025-08-07 22:31:16 +0530

    7 August

---
+++ DS/Recursion/Combinations.java
@@ -0,0 +1,45 @@
+package DS.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+// You may return the answer in any order.
+
+// Example 1:
+
+// Input: n = 4, k = 2
+// Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+// Explanation: There are 4 choose 2 = 6 total combinations.
+// Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+// Example 2:
+
+// Input: n = 1, k = 1
+// Output: [[1]]
+// Explanation: There is 1 choose 1 = 1 total combination.
+ 
+
+// Constraints:
+
+// 1 <= n <= 20
+
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(1,k,n,new ArrayList<>(),ans);
+        return ans;
+    }
+
+    static void helper(int start,int k,int n,List<Integer> curr,List<List<Integer>> ans){
+        if(curr.size()==k){
+            ans.add(new ArrayList<>(curr));
+        }
+
+        for(int i=start;i<=n;i++){
+            curr.add(i); //take
+            helper(i+1,k,n,curr,ans); //explore
+            curr.remove(curr.size()-1); //backtrack
+        }
+    }
+}
