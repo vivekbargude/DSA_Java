a31801787a02efedbac5cfb74022a825d3222a9f
vivekbargude <vivekbargude1206@gmail.com>
2025-08-15 20:30:51 +0530

    15 August

--- DS/Recursion/WordBreakII.java
+++ DS/Recursion/WordBreakII.java
@@ -67,3 +67,44 @@ public class WordBreakII {
         }
     }
 }
+
+//Approach-2 (Using simple backtracking)
+//T.C : O(2^n)
+//S.C : max depth of recursion is n (length of string s). You can say O(n) . You also have to store those sentences which is O(2^n)
+class Solution {
+    private Set<String> dict;
+    private Map<String, List<String>> memo;
+
+    private List<String> solve(String s) {
+        if (s.isEmpty()) {
+            return Arrays.asList("");
+        }
+        
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int l = 1; l <= s.length(); l++) {
+            String currWord = s.substring(0, l);
+            if (dict.contains(currWord)) {
+                String remainWord = s.substring(l);
+                List<String> remainResult = solve(remainWord);
+                for (String w : remainResult) {
+                    String toAdd = currWord + (w.isEmpty() ? "" : " ") + w;
+                    result.add(toAdd);
+                }
+            }
+        }
+
+        memo.put(s, result);
+        return result;
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        dict = new HashSet<>(wordDict);
+        memo = new HashMap<>();
+
+        return solve(s);
+    }
+}
