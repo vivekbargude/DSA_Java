ef5abd11c2d86bcf612ed5976c2ebeb76a30669a
vivekbargude <vivekbargude1206@gmail.com>
2025-08-15 19:40:26 +0530

    15 August

---
+++ DS/Recursion/WordBreakII.java
@@ -0,0 +1,69 @@
+package DS.Recursion;
+
+// Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Example 1:
+
+// Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+// Output: ["cats and dog","cat sand dog"]
+// Example 2:
+
+// Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+// Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+// Explanation: Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: []
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 20
+// 1 <= wordDict.length <= 1000
+// 1 <= wordDict[i].length <= 10
+// s and wordDict[i] consist of only lowercase English letters.
+// All the strings of wordDict are unique.
+// Input is generated in a way that the length of the answer doesn't exceed 105.
+
+import java.util.*;
+
+public class WordBreakII {
+
+    public static void main(String[] args) {
+        List<String> ans = wordBreak("catsanddog",Arrays.asList("cat","cats","and","sand","dog"));
+        System.out.println(ans);
+    }
+
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> ans = new ArrayList<>();
+        Set<String> dict = new HashSet<>(wordDict);
+
+        dfs(0, s, dict, new StringBuilder(), ans);
+        return ans;
+    }
+
+    public static void dfs(int start, String s, Set<String> dict, StringBuilder sb, List<String> ans) {
+        if(start==s.length()){
+            ans.add(sb.toString());
+            return;
+        }
+
+        for(int end = start+1; end<=s.length(); end++){
+            String sub = s.substring(start,end);
+
+            if(dict.contains(sub)){
+                int len = sb.length();
+                sb.append(sub);
+                if(end!=s.length())
+                sb.append(" ");
+                //System.out.println(sb);
+                dfs(end, s, dict, sb, ans);
+                sb.setLength(len);
+                //System.out.println(sb);
+            }
+        }
+    }
+}
